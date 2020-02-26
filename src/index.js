aa7eb02d5ebe3ea54a204f72f3b25310360c2264
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-02-26 19:32:44 +0300

    fix: add solution, belive in future solute it better

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@
 module.exports = function towelSort (matrix) {
   if(matrix == undefined || matrix.length === 0) {
     return [];
-  } else {
+  } else if(matrix.length === 1){
+    var n = 0;
+    var a = (matrix[n]).split(",");
+    return a.map(string => +string);
+  } else if(matrix.length === 2){
+    var n = 0;
+    var a = (matrix[n] + "," + matrix[n + 1].reverse()).split(",");
+    return a.map(string => +string);
+  } else if(matrix.length === 3){
     var n = 0;
     var a = (matrix[n] + "," + matrix[n + 1].reverse() + "," + matrix[n + 2]).split(",");
     return a.map(string => +string);
+  } else if(matrix.length === 4){
+    var n = 0;
+    var a = (matrix[n] + "," + matrix[n + 1].reverse() + "," + matrix[n + 2] + "," + matrix[n + 3].reverse()).split(",");
+    return a.map(string => +string);
   }
 }
+
+  /*else {
+    var n = 0;
+    var a = (matrix[n] + "," + matrix[n + 1].reverse() + "," + matrix[n + 2]).split(",");
+    return a.map(string => +string);
+  }*/
